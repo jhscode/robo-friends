7951c431c1fee0817909db6c6af1b1da51dda706
James Shin <jhscode@gmail.com>
2019-01-13 13:15:46 -0500

    Updates

---
+++ precache-manifest.7a94da80849d3d19ef91386b4601ae12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a7dc834c4f93e2bc43e",
+    "url": "/robo-friends/static/css/main.45c591ab.chunk.css"
+  },
+  {
+    "revision": "3a7dc834c4f93e2bc43e",
+    "url": "/robo-friends/static/js/main.3a7dc834.chunk.js"
+  },
+  {
+    "revision": "67dd8f0902726b451105",
+    "url": "/robo-friends/static/css/1.014d886e.chunk.css"
+  },
+  {
+    "revision": "67dd8f0902726b451105",
+    "url": "/robo-friends/static/js/1.67dd8f09.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0f7f26095e8cad8108aef7a7b6ac6db0",
+    "url": "/robo-friends/index.html"
+  }
+];

6eb9a1d8ccd9e7c66e4d3a221a74f37dee9bc22f
James Shin <jhscode@gmail.com>
2019-01-16 11:04:49 -0500

    installed redux thunk, continue to update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -11,7 +12,7 @@ import { searchRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
